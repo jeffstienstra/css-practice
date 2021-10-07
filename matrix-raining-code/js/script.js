46eb926f4d848b6733afa55441d290c42a89dd1d
jeffstienstra <jeffstienstra@gmail.com>
2021-10-07 11:25:49 -0400

    edit styling of matrix-raining-code project

--- matrix-raining-code/js/script.js
+++ matrix-raining-code/js/script.js
@@ -3,8 +3,13 @@ let canvas = document.querySelector("canvas");
 let ctx = canvas.getContext("2d");
 let width = (canvas.width = window.innerWidth);
 let height = (canvas.height = window.innerHeight);
-let str = "A+jk js:2 @dfs 17 tr YY ufds M5r P87 #18 $!& ^dfs $Ew er JH # $ * . (! ;) ,: :";
+let str = "ていは｢ﾚﾀｾﾝこリストとくリストのつべて大そされめ1234567890abcdefghijklmnopqrstuvwxyz";
 let matrix = str.split("");
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
 let font = 12;
 let col = width / font;
 let arr = [];
@@ -14,9 +19,10 @@ for (i = 0; i < col; i++) {
 }
 
 const draw = () => {
+  // let font = getRandomInt(8, 20);
   ctx.fillStyle = "rgba(0,0,0,0.05)";
   ctx.fillRect(0, 0, width, height);
-  ctx.fillStyle = "#00FF00";
+  ctx.fillStyle = "rgb(1, 250, 1)";
   ctx.font = `${font}px system-ui`;
 
   for (i = 0; i < arr.length; i++) {
@@ -30,6 +36,6 @@ const draw = () => {
   }
 };
 
-setInterval(draw, 20);
+setInterval(draw, 30);
 
 window.addEventListener("resize", () => location.reload());
